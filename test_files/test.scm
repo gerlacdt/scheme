cf269149af12bafb37f847b05bfa664d43d9c6e5
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2024-01-17 13:44:37 +0100

    added fibonacci in test file

--- test_files/test.scm
+++ test_files/test.scm
@@ -1,5 +1,6 @@
-(define double (lambda (x)
-                 (* 2 x)))
+(define fib (lambda (n)
+              (if (< n 2)
+                  1
+                  (+ (fib (- n 1)) (fib (- n 2))))))
 
-
-(double 12)
+(fib 10)
